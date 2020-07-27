0e6d73f0aa99ac4ca99dbd1720eb22d5db523ebc
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-07-27 01:26:15 -0600

    UI stubs added

--- app/src/main/java/com/dragynslayr/magicdb2/fragment/CollectionFragment.kt
+++ app/src/main/java/com/dragynslayr/magicdb2/fragment/CollectionFragment.kt
@@ -1,5 +1,20 @@
 package com.dragynslayr.magicdb2.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.dragynslayr.magicdb2.R
 
-class CollectionFragment : Fragment()
+class CollectionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.fragment_collection, container, false)
+        return v
+    }
+}
