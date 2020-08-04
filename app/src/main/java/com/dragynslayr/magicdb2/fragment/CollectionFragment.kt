9dba0213a536fd5848aa35686afcc56983d19712
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-08-03 23:49:40 -0600

    Added search to collection

--- app/src/main/java/com/dragynslayr/magicdb2/fragment/CollectionFragment.kt
+++ app/src/main/java/com/dragynslayr/magicdb2/fragment/CollectionFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dragynslayr.magicdb2.R
@@ -11,6 +12,7 @@ import com.dragynslayr.magicdb2.data.Card
 import com.dragynslayr.magicdb2.data.CardListAdapter
 import com.dragynslayr.magicdb2.data.User
 import com.dragynslayr.magicdb2.helper.DB_COLLECTION
+import com.dragynslayr.magicdb2.helper.getText
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -18,15 +20,17 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.fragment_scan.view.*
+import kotlinx.android.synthetic.main.fragment_scan.view.card_recycler
+import kotlinx.android.synthetic.main.fragment_search.view.*
 import java.util.*
 
 class CollectionFragment : Fragment() {
 
     private lateinit var v: View
-    private lateinit var cards: ArrayList<Card>
     private lateinit var user: User
     private lateinit var db: DatabaseReference
+    private lateinit var cards: ArrayList<Card>
+    private lateinit var allCards: ArrayList<Card>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,14 +43,42 @@ class CollectionFragment : Fragment() {
         user =
             activity?.intent?.extras?.getSerializable(getString(R.string.user_object_key)) as User
 
+        with(v) {
+            search_layout.editText!!.setOnKeyListener { _, _, _ ->
+                startSearch()
+                return@setOnKeyListener false
+            }
+            search_layout.editText!!.addTextChangedListener {
+                startSearch()
+            }
+        }
+
         setupResultView()
         loadCards()
 
         return v
     }
 
+    private fun startSearch() {
+        with(v) {
+            val text = search_layout.getText().toLowerCase(Locale.ROOT)
+            cards.clear()
+            if (text.isEmpty()) {
+                cards.addAll(allCards)
+            } else {
+                allCards.forEach {
+                    if (it.name!!.toLowerCase(Locale.ROOT).contains(text)) {
+                        cards.add(it)
+                    }
+                }
+            }
+            card_recycler.adapter!!.notifyDataSetChanged()
+        }
+    }
+
     private fun setupResultView() {
         cards = arrayListOf()
+        allCards = arrayListOf()
         with(v) {
             card_recycler.layoutManager = LinearLayoutManager(requireContext())
             card_recycler.adapter = CardListAdapter(cards, false)
@@ -73,6 +105,7 @@ class CollectionFragment : Fragment() {
                     cards.sortBy {
                         it.name
                     }
+                    allCards.addAll(cards)
                     v.card_recycler.adapter!!.notifyDataSetChanged()
                 }
             })
